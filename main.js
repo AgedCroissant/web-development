9e92f5956c2c889f27346d1d294621b1fd606b55
AgedCroissant <jeanarouesty@gmail.com>
2024-03-27 14:19:56 -0400

    added arrays and result() function
    added arrays that contain items that will be used to randomize the story. also added comment lines to make the code cleaner and easier to understand

--- main.js
+++ main.js
@@ -10,7 +10,17 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
+// this fuction takes an array and returns one of the items stored at random
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
   return array[random];
 }
+
+// arrays to store items used to randomize the story
+const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
+const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+
+// adds click event listener to randomize variable so result() runs once clicked
+randomize.addEventListener('click', result);
