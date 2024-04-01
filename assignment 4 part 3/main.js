4fdc2e8a70dfa281e2a3b32a74f197c731408dae
AgedCroissant <jeanarouesty@gmail.com>
2024-04-01 03:06:44 -0400

    giving the balls ability to bounce back

--- assignment 4 part 3/main.js
+++ assignment 4 part 3/main.js
@@ -29,13 +29,36 @@ class Ball {
       this.color = color;
       this.size = size;
     }
+
     // giving the balls instructions on how to act
     draw() {
         ctx.beginPath();
         ctx.fillStyle = this.color;
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
         ctx.fill();
-      }      
+    } 
+
+    // code to make the balls bounce back once theyve hit the edge of the screen
+    update() {
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+    }    
   }
   
   
